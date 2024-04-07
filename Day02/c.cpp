f30996f8627033dc67df28bc628bd8e92cffa2e3
Erik Morbach <morbacherik@gmail.com>
2024-04-07 16:28:34 -0300

    Solução mais elegante

--- Day02/c.cpp
+++ Day02/c.cpp
@@ -15,44 +15,57 @@ typedef vector<ll> vll;
 #define all(z) begin(z), end(z)
 #define EPS ((double)(1e-9))
 const int maxn = 212345;
-const int mod = 1000000000 + 7;
+const int maxp = 20;
+const int mod = 998244353;
 
+namespace sq{
+	int n;
+	int B;
+	ll values[maxn];
+	ll buck[maxn];
 
-ll v[maxn];
-ll buck[maxn];
-void solve(){
-	int n, q;
-	cin >> n >> q;
-	int B = sqrt(n);
-
-	for(int i=1;i<=n;i++){
-		cin >> v[i];
-		buck[i/B] += v[i];
+	void setup(int n){
+		memset(values, 0, sizeof(values));
+		memset(buck, 0, sizeof(buck));
+		B = sqrt(n);
+		sq::n = n;
+	}
+	void set(int i, int value){
+		buck[i/B] -= values[i];
+		buck[i/B] += (values[i] = value);
 	}
 
+	ll query(int l, int r){
+		int bL = l/B;
+		int bR = r/B;
+		ll response = 0;
+		if(bL == bR){
+			for(int i=l;i<=r;i++) response += values[i];
+			return response;
+		}
+
+		for(int i=bL+1;i < bR;i++) response += buck[i];
+		while(l/B == bL) response += values[l++];
+		while(r/B == bR) response += values[r--];
+		return response;
+	}
+}
+
+void solve(){
+	int n,q;
+	cin >> n >> q;
+	sq::setup(n);
+	for(int i=0;i<n;i++){
+		int v;
+		cin >> v;
+		sq::set(i, v);
+	}
 	for(int i=0;i<q;i++){
 		int op;
 		int a,b;
 		cin >> op >> a >> b;
-
-		if(op==1){
-			buck[a/B] += (b - v[a]);
-			v[a] = b;
-		}
-		else{
-			ll ans = 0;
-			if(b-a+1 < B){
-				for(int j=a;j<=b;j++) ans += v[j];
-				cout << ans << "\n";
-				continue;
-			}
-			int bA = (a + B - 1)/B;
-			int bB = (b/B)-1;
-			for(int j=a;j<B*bA;j++) ans += v[j];
-			for(int j=bA;j<=bB;j++) ans += buck[j];
-			for(int j=(bB+1)*B;j<=b;j++) ans += v[j];
-			cout << ans << "\n";
-		}
+		if(op==1) sq::set(a-1, b);
+		else cout << sq::query(a-1, b-1) << "\n";
 	}
 }
 
@@ -66,4 +79,5 @@ int main(){
 	}
 	return 0;
 }
+
 
